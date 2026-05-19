43cacc2c4b8c85101b1c4ebe1b1f6e92ea87de8d
D4rKing <rui.santos.maia@gmail.com>
2026-05-19 01:35:57 +0100

    feat: added Atlas class that loads spritesheet texture to vram, example spritesheet and implementation on main (#2)
    Co-authored-by: xd4rk1ng <xd4rk1ng@fake.noreply.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include "raylib.h"
+#include <raylib.h>
+#include <Atlas.hpp>
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -10,26 +11,27 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "The Game");
+    auto *atlas = Atlas::instance();
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
+        DrawTexturePro(atlas->getTexture(), atlas->getSourceRectangle(Atlas::SourceType::SPACESHIP), (Rectangle){0, 0, 200, 200}, (Vector2){8, 8}, 0, WHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -37,7 +39,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
---
+++ src/Atlas.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <raylib.h>
+#include <string_view>
+
+class Atlas
+{
+private:
+    Texture2D m_spriteSheet;
+    static Atlas *s_instance;
+    Atlas();
+    ~Atlas();
+
+public:
+    static const Atlas *instance();
+    static void destroyInstance();
+
+    enum SourceType
+    {
+        SPACESHIP = 0,
+        PLANET = 1,
+        STAR = 2,
+        DEBRIS = 3,
+    };
+    static Rectangle getSourceRectangle(SourceType sourceType);
+    const Texture2D &getTexture() const;
+};
